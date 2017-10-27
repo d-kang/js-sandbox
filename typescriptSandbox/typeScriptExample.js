5e2dba351e31b479697f4741cb5fdaf839440d0b
d-kang <david.kang714@gmail.com>
2017-10-27 15:39:06 -0700

    Convert To Class for Typescript

--- typescriptSandbox/typeScriptExample.js
+++ typescriptSandbox/typeScriptExample.js
@@ -1,12 +1,36 @@
-var _this = this;
-var render = ReactDOM.render;
-console.log('React', React);
-var App = function (props) {
-    return (React.createElement("div", null,
-        React.createElement("div", null, props.message),
-        React.createElement("div", null, props.start),
-        React.createElement("div", null, props.framework)));
-};
-render(React.createElement(App, { message: "Hello World!", start: 0, timer: setInterval(function () {
-        return console.log(_this.props.start);
-    }, 1000) }), document.getElementById('root'));
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var App = /** @class */ (function (_super) {
+    __extends(App, _super);
+    function App(props) {
+        var _this = _super.call(this, props) || this;
+        _this.state = {
+            start: _this.props.start
+        };
+        return _this;
+    }
+    App.prototype.componentDidMount = function () {
+        console.log('start', this.props.start);
+        var start = this.props.start;
+        this.props.timer(start, this.setState.bind(this));
+    };
+    App.prototype.render = function () {
+        return (React.createElement("div", null,
+            React.createElement("div", null, this.props.message),
+            React.createElement("div", null, this.state.start)));
+    };
+    return App;
+}(React.Component));
+ReactDOM.render(React.createElement(App, { message: "Hello World!", start: 0, timer: function (start, cb) {
+        return setInterval(function () {
+            return cb({ start: start++ });
+        }, 1000);
+    } }), document.getElementById('root'));
--- typescriptSandbox/typescriptExample.tsx
+++ typescriptSandbox/typescriptExample.tsx
@@ -1,24 +1,33 @@
-const { render } = ReactDOM;
-console.log('React', React);
+class App extends React.Component {
+  constructor(props: {message:string, start:number, timer:void}) {
+    super(props);
+  }
 
-const App = (props: {message:string, start:number, timer:void}) => {
-  return (
-    <div>
-      <div>{props.message}</div>
-      <div>{props.start}</div>
-      <div>{props.framework}</div>
-    </div>
-  )
+  state = {
+    start: this.props.start,
+  }
+  componentDidMount() {
+    console.log('start', this.props.start)
+    const start = this.props.start
+    this.props.timer(start, this.setState.bind(this));
+  }
+  render() {
+    return (
+      <div>
+        <div>{this.props.message}</div>
+        <div>{this.state.start}</div>
+      </div>
+    )
+  }
 }
 
 
-
-
-render(<App message="Hello World!"
+ReactDOM.render(<App message="Hello World!"
   start={0}
-  timer={
+  timer={(start, cb) =>
     setInterval(() =>
-    console.log(this.props.start),
-    1000)}
+    cb({start: start ++}),
+    1000)
+  }
   />,
   document.getElementById('root'));
