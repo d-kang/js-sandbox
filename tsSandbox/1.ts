565ef77f213edc1170372734c24800d0bc7e18a4
d-kang <david.kang714@gmail.com>
2017-10-28 16:10:50 -0700

    Function definitions for Typescript

--- tsSandbox/1.ts
+++ tsSandbox/1.ts
@@ -7,11 +7,13 @@ var lastName: string = 'Kang';
 var num1 = 100;
 var num2: number = 20;
 
-function addNumbers(n1:number, n2:number, n3:number) {
+const addNumbers : (a:number, b:number, c:string) => void
+= function addNumbers(n1, n2, n3) {
   var result = n1 + n2 + n3;
   var msg = 'Sum is = ' + result;
   console.log('msg', msg);
 }
 
-addNumbers(num1, num2, 7);
+addNumbers(num1, num2, 'o');
+
 
